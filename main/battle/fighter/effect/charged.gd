36bfc101b12ce4f59d6b66071661fdca14e0538f
kiwijuice56 <alfaroeric127@gmail.com>
2024-08-05 10:29:34 -0500

    Charged effect animation

--- main/battle/fighter/effect/charged.gd
+++ main/battle/fighter/effect/charged.gd
@@ -2,10 +2,12 @@ class_name Charged extends Effect
 
 func apply() -> void:
 	super.apply()
+	$AnimationPlayer.play("charged")
 	fighter.added_water_weakness = true
 	fighter.damage_out_multiplier += 0.25
 
 func remove() -> void:
 	super.remove()
+	$AnimationPlayer.stop()
 	fighter.added_water_weakness = false
 	fighter.damage_out_multiplier -= 0.25
